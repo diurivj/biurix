2b1e99554995a99cfaeeea195c23c0829bd6fe72
Diego Vazquez <diurivj@gmail.com>
2023-09-24 22:16:45 -0600

    adding 404 and button from shadcn

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -8,6 +8,8 @@ import {
 	SelectValue
 } from './ui/select';
 import { supportedLanguages, type Language } from '~/types/language.type';
+import { cn } from '~/lib/utils';
+import { buttonVariants } from './ui/button';
 
 export type NavContent = {
 	links: Array<{ name: string; href: string }>;
@@ -27,7 +29,21 @@ export function Nav({ content, language }: NavProps) {
 			<span className='select-none text-2xl'>&#129312;</span>
 			<nav className='flex justify-center space-x-8'>
 				{content.links.map(link => (
-					<NavLink key={link.name} to={link.href}>
+					<NavLink
+						end
+						key={link.name}
+						to={link.href}
+						className={({ isActive, isPending }) =>
+							cn(
+								buttonVariants({ variant: 'link' }),
+								isActive
+									? 'font-bold underline underline-offset-4'
+									: 'font-light',
+								isPending ? 'opacity-50' : 'opacity-100',
+								'transition-all duration-75'
+							)
+						}
+					>
 						{link.name}
 					</NavLink>
 				))}
---
+++ app/routes/$.tsx
@@ -0,0 +1,14 @@
+import { Container } from '~/components/Container';
+
+export default function NotFound() {
+	return (
+		<Container
+			as='main'
+			className='flex h-full flex-col items-center justify-center'
+		>
+			<h1 className='font-nabla select-none scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>
+				diurivj
+			</h1>
+		</Container>
+	);
+}
