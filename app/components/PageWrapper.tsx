f48731b908c28dae6563d3c4b91ddc457f7ef88c
Diego Vazquez <diurivj@gmail.com>
2023-09-24 19:46:46 -0600

    formatting

--- app/components/PageWrapper.tsx
+++ app/components/PageWrapper.tsx
@@ -2,17 +2,17 @@ import * as React from 'react';
 import { cn } from '~/lib/utils';
 
 type Props = {
-  as?: React.ElementType;
-  className?: string;
-  children: JSX.Element | JSX.Element[];
+	as?: React.ElementType;
+	className?: string;
+	children: JSX.Element | JSX.Element[];
 };
 
 export function PageWrapper({ as = 'div', className, children }: Props) {
-  return React.createElement(
-    as,
-    {
-      className: cn('mx-auto max-w-7xl px-4 sm:px-6 lg:px-8', className),
-    },
-    children
-  );
+	return React.createElement(
+		as,
+		{
+			className: cn('mx-auto max-w-7xl px-4 sm:px-6 lg:px-8', className)
+		},
+		children
+	);
 }
