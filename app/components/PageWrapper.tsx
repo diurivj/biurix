cbe1aaf6734a0990e04a4044440a6d5fb4ddd44c
Diego Vazquez <diurivj@gmail.com>
2023-09-24 19:44:07 -0600

    initial setup

---
+++ app/components/PageWrapper.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { cn } from '~/lib/utils';
+
+type Props = {
+  as?: React.ElementType;
+  className?: string;
+  children: JSX.Element | JSX.Element[];
+};
+
+export function PageWrapper({ as = 'div', className, children }: Props) {
+  return React.createElement(
+    as,
+    {
+      className: cn('mx-auto max-w-7xl px-4 sm:px-6 lg:px-8', className),
+    },
+    children
+  );
+}
